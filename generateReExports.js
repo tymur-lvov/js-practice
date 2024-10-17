ac60099f472826490c42b71b606a0ee17a9728d6
tymur-lvov <pmtl753@gmail.com>
2024-10-17 18:37:02 +0300

    Add error handling for empty relative directory case

--- generateReExports.js
+++ generateReExports.js
@@ -1,10 +1,16 @@
 import filterPathnames from './filterPathnames.js';
+import errorHandleDecor from './errorHandleDecor.js';
 import generatePathnames from './generatePathnames.js';
 import generateVariableName from './generateVariableName.js';
 import convertToRelativePath from './convertToRelativePath.js';
 
 const generateReExports = async (rootPath, relativeDir) => {
   const pathnames = await generatePathnames(rootPath, relativeDir);
+
+  if (!pathnames.length) {
+    throw new Error('All directories are empty. Nothing to re-export.');
+  }
+
   const filteredPathnames = filterPathnames(pathnames, relativeDir);
 
   return filteredPathnames.map((pathname) => {
@@ -17,4 +23,4 @@ const generateReExports = async (rootPath, relativeDir) => {
   });
 };
 
-export default generateReExports;
+export default errorHandleDecor(generateReExports);
