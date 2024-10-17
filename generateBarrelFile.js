c0293462c08283daa508f19be43ccd53eaa0f529
tymur-lvov <pmtl753@gmail.com>
2024-10-17 18:33:05 +0300

    Add error handling for invalid root and relative directory names case

--- generateBarrelFile.js
+++ generateBarrelFile.js
@@ -2,18 +2,27 @@ import path from 'path';
 import process from 'process';
 
 import injectReExports from './injectReExports.js';
+import errorHandleDecor from './errorHandleDecor.js';
 import generateReExports from './generateReExports.js';
 
-const rootPath = path.resolve('src');
-
 const generateBarrelFile = async () => {
+  const rootPath = path.resolve('src');
+
   const [relativeDir] = process.argv.slice(2);
+
+  if (!relativeDir) {
+    throw new Error('Relative directory name as an argument is required.');
+  }
+
+  if (relativeDir !== 'images' && relativeDir !== 'components') {
+    throw new Error('Relative directory name must be "images" or "components"');
+  }
 
   const reExports = await generateReExports(rootPath, relativeDir);
 
   injectReExports(reExports, relativeDir);
 };
 
-generateBarrelFile();
+const decoratedGenerateBarrelFile = errorHandleDecor(generateBarrelFile);
 
-export default generateBarrelFile;
+decoratedGenerateBarrelFile();
