4c7483f4ed620075c07a86fb35cce8beab8e1e90
tymur-lvov <pmtl753@gmail.com>
2024-10-17 23:02:49 +0300

    Create: add fn to enhance error message based on status code

--- errorHandleDecor.js
+++ errorHandleDecor.js
@@ -1,17 +1,11 @@
+import enhanceErrorMessage from './enhanceErrorMessage.js';
+
 const errorHandleDecor = (callback) => {
   return async (...args) => {
     try {
       return await callback(...args);
     } catch (error) {
-      if (error.code === 'ENOENT') {
-        const rootDir = error.path.split('/').pop();
-
-        error.message = `\"${rootDir}\" is not a correct root directory name. Expected \"src\".`;
-
-        throw error;
-      } else {
-        throw error;
-      }
+      throw enhanceErrorMessage(error);
     }
   };
 };
