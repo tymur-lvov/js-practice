f6579a7e95df11af3b0e1021672d84dba9a3ebab
tymur-lvov <pmtl753@gmail.com>
2024-11-03 17:01:47 +0200

    refactor: add re-export of function

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,3 +1,4 @@
 export { default as tryCatchDecorator } from './generateReExports/tryCatchDecorator';
 export { default as produceError } from './generateReExports/produceError';
 export { default as produceErrorMessage } from './generateReExports/produceErrorMessage';
+export { default as produceTargetPaths } from './generateReExports/produceTargetPaths';
