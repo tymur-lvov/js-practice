c278deb8e1ab1cf0b3f842d7155c319f9edde8fd
tymur-lvov <pmtl753@gmail.com>
2024-11-03 17:03:19 +0200

    refactor: change imports hierarchy

--- scripts/generateReExports/produceError.ts
+++ scripts/generateReExports/produceError.ts
@@ -1,6 +1,6 @@
-import { Reason } from '@types';
-
 import { produceErrorMessage } from '@scripts';
+
+import { Reason } from '@types';
 
 const produceError = (reason: Reason): Error => {
   const errorMessage = produceErrorMessage(reason);
