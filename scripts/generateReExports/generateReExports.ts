dfeb90b02f53cfc7cdbfd66b01207692f24ed9c9
tymur-lvov <pmtl753@gmail.com>
2024-11-05 17:47:01 +0200

    chore: extract source file paths from config file

--- scripts/generateReExports/generateReExports.ts
+++ scripts/generateReExports/generateReExports.ts
@@ -1,5 +1,18 @@
-import { tryCatchDecorator } from '@scripts';
+import * as fs from 'fs/promises';
+import * as path from 'path';
 
-const generateReExports = async () => {};
+import { produceReExports, tryCatchDecorator } from '@scripts';
+
+const generateReExports = async () => {
+  const configPath = path.resolve('generateReExports.json');
+
+  const configContent = await fs.readFile(configPath, { encoding: 'utf-8' });
+
+  const { srcFileDirs } = JSON.parse(configContent);
+
+  const reExports = produceReExports(srcFileDirs);
+
+  //  console.log(reExports);
+};
 
 tryCatchDecorator(generateReExports)();
