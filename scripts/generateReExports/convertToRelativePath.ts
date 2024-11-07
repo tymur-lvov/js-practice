f2ed0afdef8c821eee9bc40349426bf92871f8fd
tymur-lvov <pmtl753@gmail.com>
2024-11-07 19:37:39 +0200

    chore: add absolute to relative path conversion utility

---
+++ scripts/generateReExports/convertToRelativePath.ts
@@ -0,0 +1,9 @@
+const convertToRelativePath = (srcDir: string, filePath: string): string => {
+  const pathParts = filePath.split('/');
+
+  const indexOfSrcDir = pathParts.indexOf(srcDir);
+
+  return './' + pathParts.slice(indexOfSrcDir + 1).join('/');
+};
+
+export default convertToRelativePath;
