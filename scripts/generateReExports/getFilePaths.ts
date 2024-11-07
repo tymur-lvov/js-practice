b5b65e7c2c8ab2c03ef710c12403d2e0aa07351f
tymur-lvov <pmtl753@gmail.com>
2024-11-07 19:49:48 +0200

    feat: add target file paths collection utility

---
+++ scripts/generateReExports/getFilePaths.ts
@@ -0,0 +1,23 @@
+import * as fs from 'fs/promises';
+import * as path from 'path';
+
+import genrexConfig from '@config';
+
+import { processFilePath } from '@scripts';
+
+const getFilePaths = async (srcDir: string, srcDirPath: string): Promise<string[]> => {
+  const { excludedFiles: excludedFilesArray } = genrexConfig;
+
+  const excludedFiles = new Set(excludedFilesArray);
+
+  const entities = await fs.readdir(srcDirPath, { withFileTypes: true, recursive: true });
+
+  const filePaths = entities
+    .filter((entity) => entity.isFile() && !excludedFiles.has(entity.name))
+
+    .map((file) => path.resolve(file.parentPath, file.name));
+
+  return filePaths.map((filePath) => processFilePath(srcDir, filePath));
+};
+
+export default getFilePaths;
