843689c1bbc04746375ef2ed71d53d22384598c0
tymur-lvov <pmtl753@gmail.com>
2024-11-07 19:36:25 +0200

    chore: add various re-exports

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,4 +1,8 @@
 export { default as tryCatchDecorator } from './generateReExports/tryCatchDecorator';
-export { default as produceError } from './generateReExports/produceError';
-export { default as produceErrorMessage } from './generateReExports/produceErrorMessage';
 export { default as getSrcDir } from './generateReExports/getSrcDir';
+export { default as getStatements } from './generateReExports/getStatements';
+export { default as getFilePaths } from './generateReExports/getFilePaths';
+export { default as convertToRelativePath } from './generateReExports/convertToRelativePath';
+export { default as processFilePath } from './generateReExports/processFilePath';
+export { default as removeExtension } from './generateReExports/removeExtension';
+export { default as ReExport } from './classes/ReExport';
