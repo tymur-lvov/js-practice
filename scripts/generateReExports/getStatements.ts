172932f6247e082c563c4d41c988a4f27938ff1d
tymur-lvov <pmtl753@gmail.com>
2024-11-08 15:17:49 +0200

    chore: add variable names extraction utility

--- scripts/generateReExports/getStatements.ts
+++ scripts/generateReExports/getStatements.ts
@@ -1,7 +1,10 @@
-import { getFilePaths } from '@scripts';
+import { getFilePaths, getVariableNames } from '@scripts';
 
 const getStatements = async (srcDir: string, srcDirPath: string) => {
   const filePaths = await getFilePaths(srcDir, srcDirPath);
+
+  const variableNames = getVariableNames(filePaths);
+  console.log(variableNames);
 };
 
 export default getStatements;
--- scripts/index.ts
+++ scripts/index.ts
@@ -1,3 +1,4 @@
+export { default as ReExport } from './classes/ReExport';
 export { default as tryCatchDecorator } from './generateReExports/tryCatchDecorator';
 export { default as getSrcDir } from './generateReExports/getSrcDir';
 export { default as getStatements } from './generateReExports/getStatements';
@@ -5,4 +6,4 @@ export { default as getFilePaths } from './generateReExports/getFilePaths';
 export { default as convertToRelativePath } from './generateReExports/convertToRelativePath';
 export { default as processFilePath } from './generateReExports/processFilePath';
 export { default as removeExtension } from './generateReExports/removeExtension';
-export { default as ReExport } from './classes/ReExport';
+export { default as getVariableNames } from './generateReExports/getVariableNames';
