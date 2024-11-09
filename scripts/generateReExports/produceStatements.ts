25efc2a8aaa2e592beab0c98e1f782d62b9ccb67
tymur-lvov <pmtl753@gmail.com>
2024-11-09 20:05:04 +0200

    chore: change var name validation to file path

--- scripts/generateReExports/produceStatements.ts
+++ scripts/generateReExports/produceStatements.ts
@@ -2,7 +2,7 @@ const produceStatements = (filePaths: string[], varNames: string[]): string[] =>
   return varNames.map((varName, index) => {
     const filePath = filePaths[index];
 
-    return varName.includes('_types')
+    return filePath.includes('.types')
       ? `export type { default as ${varName} } from '${filePath}';`
       : `export { default as ${varName} } from '${filePath}';`;
   });
