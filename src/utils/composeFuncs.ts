8633d4fef99e913ce6b48a98a7bbe1a71e09c8ed
tymur-lvov <pmtl753@gmail.com>
2024-11-11 12:59:53 +0200

    chore: add multiple args support for functions composition utility

--- src/utils/composeFuncs.ts
+++ src/utils/composeFuncs.ts
@@ -2,7 +2,9 @@ import { IFunc } from '@types';
 
 const composeFuncs = <T extends any[], R>(...constituentFuncs: T): IFunc => {
   return (...args: T): R => {
-    return constituentFuncs.reduce((funcResult, currentFunc) => currentFunc(funcResult), args);
+    return constituentFuncs.reduce((funcResult, currentFunc) => {
+      return Array.isArray(funcResult) ? currentFunc(...funcResult) : currentFunc(funcResult);
+    }, args);
   };
 };
 
