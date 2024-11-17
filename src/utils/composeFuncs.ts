628a109bdadb6fabfc37488ee99e590c7dde6280
tymur-lvov <pmtl753@gmail.com>
2024-11-17 17:06:38 +0200

    refactor: change compose functions utility to support single argument

--- src/utils/composeFuncs.ts
+++ src/utils/composeFuncs.ts
@@ -1,9 +1,9 @@
 import { IFunc } from '@types';
 
 const composeFuncs = <T extends any[], R>(...constituentFuncs: T): IFunc => {
-  return (...args: T): R => {
-    return constituentFuncs.reduce((funcResult, currentFunc) => {
-      return Array.isArray(funcResult) ? currentFunc(...funcResult) : currentFunc(funcResult);
+  return (args: T): R => {
+    return constituentFuncs.reduce((prevResult, currentFunc) => {
+      return currentFunc(prevResult);
     }, args);
   };
 };
