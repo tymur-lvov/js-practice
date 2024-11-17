0b20d0227367fdfbb2db8bf33a7e62178914c229
tymur-lvov <pmtl753@gmail.com>
2024-11-17 18:37:31 +0200

    fix: temporary fix

--- src/utils/genrex/processFilePaths.ts
+++ src/utils/genrex/processFilePaths.ts
@@ -1,12 +1,20 @@
-import { composeFuncs, getBasename, getRelativePath, removeExtension } from '@utils';
+import { composeFuncs, getBasename, removeExtension } from '@utils';
 
 const processFilePaths = (srcDirPath: string, filePaths: string[]): string[] => {
   const srcDir = getBasename(srcDirPath);
 
+  const getRelativePath = (filePath: string): string => {
+    const pathParts = filePath.split('/');
+
+    const srcDirIndex = pathParts.indexOf(srcDir);
+
+    return './' + pathParts.slice(srcDirIndex + 1).join('/');
+  };
+
   return filePaths.map((filePath) => {
     const getRelativePathsWithoutExtensions = composeFuncs(getRelativePath, removeExtension);
 
-    return getRelativePathsWithoutExtensions(srcDir, filePath);
+    return getRelativePathsWithoutExtensions(filePath);
   });
 };
 
