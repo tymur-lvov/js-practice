4d3cead4803d0ebc3f3007e7b7ab3294c32e1f21
tymur-lvov <pmtl753@gmail.com>
2024-11-20 22:11:00 +0200

    refactor: add multiple arguments support

--- src/helpers/pathHelpers.ts
+++ src/helpers/pathHelpers.ts
@@ -1,5 +1,5 @@
 import * as path from 'path';
 
-export const transformPathToAbsolute = (relativePath: string): string => {
-  return path.resolve(relativePath);
+export const getAbsolutePath = (...pathParts: string[]): string => {
+  return path.resolve(...pathParts);
 };
