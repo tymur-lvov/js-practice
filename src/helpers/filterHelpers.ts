6476252b61a2ead371cab10be738fdcea0e6f70d
tymur-lvov <pmtl753@gmail.com>
2024-11-21 15:54:20 +0200

    chore: add utility to filter files to be include in export

--- src/helpers/filterHelpers.ts
+++ src/helpers/filterHelpers.ts
@@ -1,5 +1,13 @@
-import { Dirent } from 'fs';
+import { getConfigProp } from '@helpers';
+
+import type { Dirent } from 'fs';
+
+const filesToExclude = new Set(getConfigProp('filesToExclude'));
 
 export const filterFiles = (dirEnts: Dirent[]): Dirent[] => {
   return dirEnts.filter((dirEnt) => dirEnt.isFile());
+};
+
+export const filterFilesToInclude = (files: Dirent[]): Dirent[] => {
+  return files.filter((file) => !filesToExclude.has(file.name));
 };
