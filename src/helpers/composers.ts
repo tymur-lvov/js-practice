e45422a952f90bfa208b833817ad0beabe7191d7
tymur-lvov <pmtl753@gmail.com>
2024-11-24 15:39:46 +0200

    chore: change types

--- src/helpers/composers.ts
+++ src/helpers/composers.ts
@@ -1,13 +1,13 @@
-export const compose = (...funcs: ((arg: any) => any)[]): ((arg: any) => any) => {
-  return (arg) => {
+export const compose = (...funcs: ((arg: any) => any)[]) => {
+  return (arg: any): any => {
     return funcs.reduce((result, func) => {
       return func(result);
     }, arg);
   };
 };
 
-export const asyncCompose = (...funcs: ((arg: any) => any)[]): ((arg: any) => Promise<any>) => {
-  return async (arg) => {
+export const asyncCompose = (...funcs: ((arg: any) => any)[]) => {
+  return async (arg: any | Promise<any>): Promise<any> => {
     let result = await arg;
 
     for (const func of funcs) {
