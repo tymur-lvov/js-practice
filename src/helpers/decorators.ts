9aa2123d8da2a6630febce9e9405ad3fa929e287
tymur-lvov <pmtl753@gmail.com>
2024-11-26 15:34:58 +0200

    chore: add async operations processing support

--- src/helpers/decorators.ts
+++ src/helpers/decorators.ts
@@ -1,9 +1,17 @@
 import type { IFunc } from '@types';
 
-export const mapDecor = (func: IFunc): IFunc => {
-  const funcRecurs = (arg: any): any => {
-    return !Array.isArray(arg) ? func(arg) : arg.map(funcRecurs);
+const mapDecorCreator =
+  (mode: 'async' | 'sync'): IFunc =>
+  (func: IFunc): IFunc => {
+    const funcRecurs = (arg: any): any => {
+      if (!Array.isArray(arg)) {
+        return func(arg);
+      } else {
+        return mode === 'async' ? Promise.all(arg.map(funcRecurs)) : arg.map(funcRecurs);
+      }
+    };
+    return funcRecurs;
   };
 
-  return funcRecurs;
-};
+export const mapDecor = mapDecorCreator('sync');
+export const asyncMapDecor = mapDecorCreator('async');
