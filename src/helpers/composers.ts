fd528322bd12b1467590477f5df1f30029d05595
tymur-lvov <pmtl753@gmail.com>
2024-11-26 15:34:02 +0200

    refactor: add more explicit return

--- src/helpers/composers.ts
+++ src/helpers/composers.ts
@@ -4,9 +4,11 @@ const createComposer =
   (mode: 'async' | 'sync'): IComposer =>
   (...funcs: IFunc[]): IFunc =>
   (arg: any): Promise<any> | any => {
-    return mode === 'async'
-      ? funcs.reduce(async (result, func) => func(await result), arg)
-      : funcs.reduce((result, func) => func(result), arg);
+    if (mode === 'async') {
+      return funcs.reduce(async (result, func) => func(await result), arg);
+    } else {
+      return funcs.reduce((result, func) => func(result), arg);
+    }
   };
 
 export const compose = createComposer('sync');
