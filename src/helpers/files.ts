e98a008c67877ff51e3f8d7f21789811b31a2525
tymur-lvov <pmtl753@gmail.com>
2024-11-26 18:11:00 +0200

    chore: restructure main process

--- src/helpers/files.ts
+++ src/helpers/files.ts
@@ -1,13 +1,11 @@
 import * as fs from 'fs/promises';
 
-import { asyncMapDecor } from '@helpers';
-
 import type { Dirent } from 'fs';
 
-export const getDirEntsRecurs = asyncMapDecor((dirPath: string): Promise<Dirent[]> => {
+export const getDirEntsRecurs = (dirPath: string): Promise<Dirent[]> => {
   return fs.readdir(dirPath, { withFileTypes: true, recursive: true });
-});
+};
 
-export const getFileData = asyncMapDecor((filePath: string): Promise<string> => {
+export const getFileData = (filePath: string): Promise<string> => {
   return fs.readFile(filePath, 'utf-8');
-});
+};
--- src/helpers/paths.ts
+++ src/helpers/paths.ts
@@ -1,13 +1,11 @@
 import * as path from 'path';
 
-import { mapDecor } from '@helpers';
-
 import type { Dirent } from 'fs';
 
-export const getAbsolutePath = mapDecor((absolutePath: string): string => {
+export const getAbsolutePath = (absolutePath: string): string => {
   return path.resolve(absolutePath);
-});
+};
 
-export const getDirEntPath = mapDecor(({ parentPath, name }: Dirent): string => {
+export const getDirEntPath = ({ parentPath, name }: Dirent): string => {
   return path.resolve(parentPath, name);
-});
+};
