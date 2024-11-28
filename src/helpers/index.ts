076776a04540a149ffd8006e6b4c6228ab61a62b
tymur-lvov <pmtl753@gmail.com>
2024-11-28 22:25:14 +0200

    refactor: rename variables

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,7 +3,7 @@ export { asyncCompose } from './composers';
 export { getConfigOption } from './misc';
 export { getDirEntsRecurs } from './files';
 export { getAbsolutePath } from './paths';
-export { filterFiles } from './filters';
+export { filterFilesFromDirEnts } from './filters';
 export { filterFilesToInclude } from './filters';
 export { getDirEntPath } from './paths';
 export { getFileData } from './files';
