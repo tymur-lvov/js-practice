e68d78dee539df5f304782f18af00cc7e2e182d0
tymur-lvov <pmtl753@gmail.com>
2024-11-28 17:59:46 +0200

    chore: main process v. 0.1

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,33 @@
-// Собираем массив объектов - экспорт модулей, с полями: filePath, fileData;
-// Фильтруем массив объектов по значениям нужных полей;
+import * as path from 'path';
+
+import { asyncCompose, filterFiles, getConfigOption, getDirEntsRecurs } from '@helpers';
+
+import type { Dirent } from 'fs';
+
+const main = async () => {
+  const reExportFiles = await createReExportFiles();
+  console.log(reExportFiles);
+};
+
+const createReExportFiles = async () => {
+  const targetDirPaths = getConfigOption('targetDirPaths');
+
+  const files = await getFiles(targetDirPaths);
+
+  return files.map((files, idx) => ({
+    srcPath: createSrcPath(targetDirPaths[idx]),
+    fileData: files.map(getFileData),
+  }));
+};
+
+const getFiles = async (dirPaths: string[]) => {
+  return Promise.all(dirPaths.map(asyncCompose(getDirEntsRecurs, filterFiles)));
+};
+
+const createSrcPath = (dirPath: string) => {
+  return `${path.resolve(dirPath)}/index.ts`;
+};
+
+const getFileData = (file: Dirent) => {};
+
+main();
