24333879b129137bc2d25ebd0d41f297ba7ded29
tymur-lvov <pmtl753@gmail.com>
2024-12-14 18:10:02 +0200

    refactor: FP principles adaptation v. 0.4

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,3 @@
-import config from './config.json';
-import { processIndexFile } from './processors/processIndexFile';
-
-const main = async () => {
-  const { parentPaths } = config;
-
-  console.log(await Promise.all(parentPaths.map(processIndexFile)));
-};
+const main = async () => {};
 
 main();
