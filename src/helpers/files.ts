19d0cfad3bdf4150621a65cd4f0f8681706e24ba
tymur-lvov <pmtl753@gmail.com>
2024-12-25 18:30:09 +0200

    refactor: FP principles adaptation v. 0.12

--- src/helpers/files.ts
+++ src/helpers/files.ts
@@ -1,7 +1,4 @@
 import { readdir } from 'fs/promises';
-import { concatExportStatement, getExportStatement, getVarName } from './strings';
-import { getFilePaths, getRelativePath } from './paths';
-import { indexFileName, typesIndexFileName } from '../constants';
 
 export const getDirEntsRecurs = async (parentPath) => {
   return readdir(parentPath, { withFileTypes: true, recursive: true });
