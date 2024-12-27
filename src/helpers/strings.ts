02fee5047d9e568b1b3d0dd7a80e69c0a04a32b7
tymur-lvov <pmtl753@gmail.com>
2024-12-27 18:17:27 +0200

    refactor: FP principles adaptation v. 0.13

--- src/helpers/strings.ts
+++ src/helpers/strings.ts
@@ -1,38 +1,33 @@
+import { compose } from './composers';
 import { findExportStatement } from './finders';
 import { getBasename, sterilizeBasename } from './paths';
 
-export const removeExtension = (filePath) => {
+export const deleteExtension = (filePath) => {
   return filePath.slice(0, filePath.lastIndexOf('.'));
 };
 
 export const getVarName = (filePath) => {
-  const filePathBasename = getBasename(filePath);
-
-  const basenameWithoutExtension = removeExtension(filePathBasename);
-
-  return sterilizeBasename(basenameWithoutExtension);
+  return sterilizeBasename(compose(getBasename, deleteExtension)(filePath));
 };
 
-export const splitStringsToParts = (div, ...strings) => {
+export const splitToParts = (div, ...strings) => {
   return strings.map((string) => string.split(div));
 };
 
 export const getParentDirIndex = (parentPathParts, filePathParts) => {
-  const parentDir = parentPathParts[parentPathParts.length - 1];
-
-  return filePathParts.indexOf(parentDir);
+  return filePathParts.indexOf(parentPathParts[parentPathParts.length - 1]);
 };
 
 export const getNamedExportStatement = (realtivePath) => {
-  return `export * from ${removeExtension(realtivePath)};\n`;
+  return `export * from ${deleteExtension(realtivePath)};\n`;
 };
 
 export const getNamedTypeExportStatement = (realtivePath) => {
-  return `export type * from ${removeExtension(realtivePath)};\n`;
+  return `export type * from ${deleteExtension(realtivePath)};\n`;
 };
 
 export const getDefaultExportStatement = (varName, realtivePath) => {
-  return `export { default as ${varName} } from ${removeExtension(realtivePath)};\n`;
+  return `export { default as ${varName} } from ${deleteExtension(realtivePath)};\n`;
 };
 
 export const getExportStatement = (varName, realtivePath) => {
