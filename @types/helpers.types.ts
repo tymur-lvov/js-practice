4125dc5e5a0e63d452d98f5321e6823fdfeefd21
tymur-lvov <pmtl753@gmail.com>
2024-12-30 19:58:48 +0200

    refactor: FP principles adaptation v. 0.16

--- @types/helpers.types.ts
+++ @types/helpers.types.ts
@@ -1,5 +1,10 @@
 import type { Dirent } from 'fs';
 
+export interface ConfigInt {
+  parentPaths: string[];
+  itemsToExclude: string[];
+}
+
 export interface IndexFileInt {
   indexFilePath: string;
   indexFileData: string;
@@ -36,21 +41,51 @@ export type GetNamedExportStatementType = (realtivePath: string) => string;
 
 export type ReadFileDataType = (filePath: string) => Promise<string>;
 
-export type GetIndexFileNameType = () => string;
+export type WriteFileDataType = (filePath: string, fileData: string) => Promise<void>;
 
-export type GetTypesIndexFileNameType = () => string;
+export type WriteIndexFilesType = (indexFiles: IndexFileInt[]) => void;
 
-//
+export type CreateIndexFileDataType = (parentPath: string, modulePaths: string[]) => string;
 
-export type ComposeType = (funcs: (arg: any) => any[]) => (arg: any) => any;
+export type GetIndexFileDataType = (
+  parentPath: string
+) => Promise<ReturnType<CreateIndexFileDataType>>;
 
-export type AsyncComposeType = (funcs: (arg: any) => any[]) => (arg: any) => Promise<any>;
+export type GetDirEntsRecursType = (parentPath: string) => Promise<Dirent[]>;
+
+export type GetDirEntDataType = (dirEnt: Dirent) => ReturnType<FindDirEntDataType>;
+
+export type FindDirEntDataType = (
+  dirEnt: Dirent,
+  dirEntPath: string
+) => ReturnType<DirEntDataConditionInt['getResult']>;
+
+export type FindIndexFileNameType = (
+  parentPath: string
+) => ReturnType<IndexFileNameConditionInt['getResult']>;
+
+export type FindExportStatementType = (
+  varName: string,
+  parentPath: string
+) => ReturnType<ExportStatementConditionInt['getResult']>;
+
+export type ComposeType = (...funcs: ((arg: any) => any)[]) => (arg: any) => any;
+
+export type AsyncComposeType = (...funcs: ((arg: any) => any)[]) => (arg: any) => Promise<any>;
 
 export type CreateIndexFilesType = (parentPaths: string[]) => Promise<IndexFileInt[]>;
 
 export type AssignDirEntDataType = (dirEnts: Dirent[]) => Promise<DirEntInt[]>;
 
 export type GetIndexFileNameConditionsType = (parentPath: string) => IndexFileNameConditionInt[];
+
+export type GetParentPathsType = () => ConfigInt['parentPaths'];
+
+export type GetItemsToExcludeType = () => ConfigInt['itemsToExclude'];
+
+export type GetIndexFileNameType = () => string;
+
+export type GetTypesIndexFileNameType = () => string;
 
 export type GetDirEntDataConditionsType = (
   dirEnt: Dirent,
@@ -61,3 +96,32 @@ export type GetExportStatementConditionsType = (
   varName: string,
   realtivePath: string
 ) => ExportStatementConditionInt[];
+
+export type FilterFilesType = (dirEnts: DirEntInt[]) => DirEntInt[];
+
+export type FilterFilesToIncludeType = (dirEnts: DirEntInt[]) => DirEntInt[];
+
+export type FilterModulesType = (dirEnts: DirEntInt[]) => DirEntInt[];
+
+export type IsFileAModuleType = (fileName: string, fileData: string) => boolean;
+
+export type AppendDotAndSlashType = (filePath: string) => string;
+
+export type GetPathType = (parentPath: string, name: string) => string;
+
+export type GetBasenameType = (filePath: string) => string;
+
+export type SterilizeBasenameType = (basename: string) => string;
+
+export type GetIndexFilePathType = (parentPath: string) => ReturnType<GetPathType>;
+
+export type GetRelativePathType = (
+  parentPath: string,
+  filePath: string
+) => ReturnType<AppendDotAndSlashType>;
+
+export type GetTargetFilesType = (parentPath: string) => Promise<ReturnType<FilterModulesType>>;
+
+export type GetFilePathsType = (parentPath: string) => Promise<ReturnType<GetPathType>[]>;
+
+export type SliceFromParentDirType = (parentPath: string, filePath: string) => string;
