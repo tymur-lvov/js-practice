e3ea047300168fa2859307e179ce8fdab8e828f2
tymur-lvov <pmtl753@gmail.com>
2024-12-30 19:58:59 +0200

    refactor: FP principles adaptation v. 0.16

--- src/helpers/filters.ts
+++ src/helpers/filters.ts
@@ -1,13 +1,18 @@
+import type {
+  FilterFilesToIncludeType,
+  FilterFilesType,
+  FilterModulesType,
+} from '../../@types/helpers.types';
 import { isEntityAFile, isFileAModule, isFileToBeIncluded } from './predicates';
 
-export const filterFiles = (dirEnts) => {
+export const filterFiles: FilterFilesType = (dirEnts) => {
   return dirEnts.filter(({ dirEntInfo }) => isEntityAFile(dirEntInfo));
 };
 
-export const filterFilesToInclude = (dirEnts) => {
+export const filterFilesToInclude: FilterFilesToIncludeType = (dirEnts) => {
   return dirEnts.filter(({ dirEntInfo }) => isFileToBeIncluded(dirEntInfo));
 };
 
-export const filterModules = (dirEnts) => {
+export const filterModules: FilterModulesType = (dirEnts) => {
   return dirEnts.filter(({ dirEntInfo, dirEntData }) => isFileAModule(dirEntInfo.name, dirEntData));
 };
