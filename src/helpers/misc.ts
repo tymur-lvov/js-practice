9f72b0b83fff869c3fbbda7829ecd12e362e7ba2
tymur-lvov <pmtl753@gmail.com>
2024-12-30 19:59:14 +0200

    refactor: FP principles adaptation v. 0.16

--- src/helpers/misc.ts
+++ src/helpers/misc.ts
@@ -1,18 +1,24 @@
+import type {
+  GetIndexFileNameType,
+  GetItemsToExcludeType,
+  GetParentPathsType,
+  GetTypesIndexFileNameType,
+} from '../../@types/helpers.types';
 import config from '../config.json';
 import { indexFileName, typesIndexFileName } from '../constants';
 
-export const getParentPaths = () => {
+export const getParentPaths: GetParentPathsType = () => {
   return config.parentPaths;
 };
 
-export const getItemsToExclude = () => {
+export const getItemsToExclude: GetItemsToExcludeType = () => {
   return config.itemsToExclude;
 };
 
-export const getIndexFileName = () => {
+export const getIndexFileName: GetIndexFileNameType = () => {
   return indexFileName;
 };
 
-export const getTypesIndexFileName = () => {
+export const getTypesIndexFileName: GetTypesIndexFileNameType = () => {
   return typesIndexFileName;
 };
