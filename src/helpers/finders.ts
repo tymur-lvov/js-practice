bfd1eda456f12d0f0fe5750ac479432453c815ad
tymur-lvov <pmtl753@gmail.com>
2024-12-30 19:59:07 +0200

    refactor: FP principles adaptation v. 0.16

--- src/helpers/finders.ts
+++ src/helpers/finders.ts
@@ -1,13 +1,43 @@
-import { getExportStatementConditions, getIndexFileNameConditions } from './conditions';
+import type {
+  FindDirEntDataType,
+  FindExportStatementType,
+  FindIndexFileNameType,
+} from '../../@types/helpers.types';
+import {
+  getDirEntDataConditions,
+  getExportStatementConditions,
+  getIndexFileNameConditions,
+} from './conditions';
 
-export const findIndexFileName = (parentPath) => {
+export const findIndexFileName: FindIndexFileNameType = (parentPath) => {
   const conditions = getIndexFileNameConditions(parentPath);
+  const result = conditions.find(({ checkCondition }) => checkCondition())?.getResult();
 
-  return conditions.find(({ checkCondition }) => checkCondition()).getResult();
+  if (!result) {
+    throw new Error('Index file name condition result not found');
+  }
+
+  return result;
 };
 
-export const findExportStatement = (varName, realtivePath) => {
+export const findExportStatement: FindExportStatementType = (varName, realtivePath) => {
   const conditions = getExportStatementConditions(varName, realtivePath);
+  const result = conditions.find(({ checkCondition }) => checkCondition())?.getResult();
 
-  return conditions.find(({ checkCondition }) => checkCondition()).getResult();
+  if (!result) {
+    throw new Error('Export condition result not found');
+  }
+
+  return result;
+};
+
+export const findDirEntData: FindDirEntDataType = (dirEnt, dirEntPath) => {
+  const conditions = getDirEntDataConditions(dirEnt, dirEntPath);
+  const result = conditions.find(({ checkCondition }) => checkCondition())?.getResult();
+
+  if (result === undefined) {
+    throw new Error('DirEnt data condition result not found');
+  }
+
+  return result;
 };
